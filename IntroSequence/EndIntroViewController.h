880eb9e538d3699a32ad8c70443e8f3428a5f858
Sarah Allen <sarah@ultrasaurus.com>
2015-06-05 19:10:47 -0700

    intro sequence storyboard:

--- IntroSequence/EndIntroViewController.h
+++ IntroSequence/EndIntroViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  EndIntroViewController.h
 //  IntroSequence
 //
 //  Created by Sarah Allen on 6/5/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface EndIntroViewController : UIViewController
 
 @end
-
