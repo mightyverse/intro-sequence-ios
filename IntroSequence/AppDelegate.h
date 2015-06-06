bc912f27cf92fcf7c5a0e780d33774d9c545f441
Sarah Allen <sarah@ultrasaurus.com>
2015-06-05 18:39:01 -0700

    Initial Commit

---
+++ IntroSequence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntroSequence
+//
+//  Created by Sarah Allen on 6/5/15.
+//  Copyright (c) 2015 Mightyverse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
